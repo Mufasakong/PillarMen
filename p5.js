3ef34177715c04e109c3ff3287ac33ec14680fe5
Mufasakong <54103682+Mufasakong@users.noreply.github.com>
2020-12-20 22:17:53 +0100

    Update p5.js

--- p5.js
+++ p5.js
@@ -1,17 +1,20 @@
 let talList;
 let rand; 
 
+//Vi har en liste og der bliver shufflet
 function setup(){
   createCanvas(400,400);
   talList = [1,2,3,4,5,6,7,8,9,10]; 
   rand = shuffle(talList);
 }
 
+// Vi får tegnet vores diagram på tilfældigt vis
 function draw(){
   background(220);
   drawDiagram(rand);  
 }
 
+//Her kommer det deklarative programmering på banen
 function drawDiagram(list){
   list.filter(function(current,index,arr)
   {rect(10+35*index,400,30,-(current)*30)
