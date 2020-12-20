9f5b156ffc948d2aa431dc17287e3fd2a4f0c72e
Mufasakong <54103682+Mufasakong@users.noreply.github.com>
2020-12-20 22:40:01 +0100

    Update p5.js

--- p5.js
+++ p5.js
@@ -1,19 +1,30 @@
 let talList;
 let rand; 
+let rystPosen = false;
 
 //Vi har en liste og der bliver shufflet
 function setup(){
   createCanvas(400,400);
   talList = [1,2,3,4,5,6,7,8,9,10]; 
-  rand = shuffle(talList);
+  rand = talList;
 }
 
 // Vi får tegnet vores diagram på tilfældigt vis
 function draw(){
   background(220);
-  drawDiagram(rand);  
+  drawDiagram(rand);
+  console.log(rystPosen);
 }
 
+// Sorteret eller ikke sorteret - det er spørgsmålet! 
+function mousePressed(){
+  rystPosen = !rystPosen;
+  if (rystPosen) {
+      rand = shuffle(talList);
+      } else {
+      rand = talList;
+      }
+}
 //Her kommer det deklarative programmering på banen
 function drawDiagram(list){
   list.filter(function(current,index,arr)
